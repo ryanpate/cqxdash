d1859ba9421a82a7d1828200dff9365c9fd0c458
Ryan Pate <rryan.pate@att.net>
2025-08-20 14:05:00 -0500

    updated server settings for API fix

--- app.py
+++ app.py
@@ -750,4 +750,4 @@ if __name__ == '__main__':
     print("-" * 50)
 
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='127.0.0.1', port=port, debug=False, threaded=True)
+    app.run(debug=False, host='0.0.0.0', port=5000)
